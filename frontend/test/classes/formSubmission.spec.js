0e4cf92498b89d8de6532c0a59007064a74e8db5
rileylittlefield <rlittlefield@manifestcorp.com>
2016-12-23 17:16:10 -0500

    adds form submission unit test

--- frontend/test/classes/formSubmission.spec.js
+++ frontend/test/classes/formSubmission.spec.js
@@ -1,9 +1,45 @@
+import Form from '../../src/submodules/authentication/classes/form.js';
+import FormField from '../../src/submodules/authentication/classes/formField.js';
+import MatchingFormField from '../../src/submodules/authentication/classes/matchingFormField.js';
+
+import FormSubmission from '../../src/submodules/authentication/classes/formSubmission.js';
+import FormValidation from '../../src/submodules/authentication/classes/formValidation.js';
+import FormAuthor from '../../src/submodules/authentication/classes/formAuthor.js';
+
 describe("FormSubmission", () => {
+  let form, formField, formSubmitter;
+
+  beforeEach(() => {
+    form = new Form({
+      FormAuthor: FormAuthor,
+      FormSubmission: FormSubmission,
+      FormValidation: FormValidation
+    });
+
+    formField = new FormField({
+      name: "fieldName",
+      errorChecks: [],
+      type: "type",
+      icon: "icon"
+    });
+
+    formSubmitter = form.submitter;
+  });
+
   describe("#constructor", () => {
-    xit("should instantiate with an instance of a form")
+    it("should instantiate with an instance of a form", () => {
+      expect(formSubmitter.form).toEqual(jasmine.any(Form));
+    })
   });
 
   describe("#submissionReport", () => {
-    xit("should return an obj containing field names pointing to field values")
+    it("should return an obj containing field names pointing to field values", () => {
+      formField.value = "sampleValue"
+      form.addField(formField);
+
+      let submissionReport = form.submissionReport();
+      expect(submissionReport.fieldName).toEqual("sampleValue");
+      expect(Object.keys(submissionReport).length).toEqual(1);
+    });
   });
 });
