e9cedbb49bbf03180e0710523b24c2bb8e690fb7
rileylittlefield <rlittlefield@manifestcorp.com>
2016-12-27 10:45:16 -0500

    deletes debugger break points

--- frontend/src/submodules/authentication/services/credentialsSubmissionCBs.js
+++ frontend/src/submodules/authentication/services/credentialsSubmissionCBs.js
@@ -2,13 +2,11 @@ function CredentialsSubmissionCBs (authenticationStore, $state) {
 
   return ({
     successCB: (response) => {
-      debugger
       authenticationStore.setCurrentUser(response.data.user);
       $state.go('dashboard');
     },
 
     failureCB: (response) => {
-      debugger
       alert("A login error Occurred");
     },
   });
